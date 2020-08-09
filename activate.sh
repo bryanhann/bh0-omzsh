1e96d413d1ad90775a4fbdb366a812d50e8ada39
Bryan Hann <nobody@nowhere>
2020-08-09 16:17:09 +0800

    Configure for bh0.

--- activate.sh
+++ activate.sh
@@ -1,17 +1,5 @@
-export                OMZSH_ROOT_1234_MAGIC=$(dirname $0)
-omzsh_root () { echo $OMZSH_ROOT_1234_MAGIC ; }
-
-omzsh_zsh  () { echo $(omzsh_root)/BLD.ZSH ; }
-omzsh_remove () { rm -rf $(omzsh_zsh) ; }
-omzsh_install () { 
-    [[ -d $ZSH ]] && bh0note OMZSH is already installed
-    [[ -d $ZSH ]] && return
-    [[ -d $ZSH ]] || bh0note Installing OMZSH
-    [[ -d $ZSH ]] || $(omzsh_root)/vendor/install.sh --unattended
-    [[ -d $ZSH ]] || echo OMZSH install failed
-    [[ -d $ZSH ]] || echo OMZSH install succeded
-}
-
-
-bh0export ZSH	$(omzsh_root)/BLD.ZSH 
+echo 777
+read
+bh0export ZSH   $(dirname $0)/ZSH
+[[ -d $ZSH ]] || $(dirname $0)/vendor/install.sh --unattended
 
